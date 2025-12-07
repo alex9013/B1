0ab3fd745075cb638ceba1d4f43b515f18abf661
alex9013 <2122100674@soy.utj.edu.mx>
2025-12-06 21:12:13 -0600

    B10

--- api/index.js
+++ api/index.js
@@ -2,8 +2,28 @@
 import "dotenv/config";
 import app from "../src/app.js";
 
-// Vercel necesita que exportes el handler así
+// Vercel serverless function handler
 export default async (req, res) => {
-  // Deja que Express maneje la request
+  // Configurar headers CORS manualmente antes de que Express los maneje
+  const origin = req.headers.origin;
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://f1-indol-theta.vercel.app"
+  ];
+  
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  }
+  
+  // Si es una petición OPTIONS, responder inmediatamente
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  // Dejar que Express maneje el resto
   return app(req, res);
 };

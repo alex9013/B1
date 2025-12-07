ca25880a6af6e17d37fcb22eac80df413de30175
alex9013 <2122100674@soy.utj.edu.mx>
2025-12-06 21:22:51 -0600

    B11

--- api/index.js
+++ api/index.js
@@ -1,29 +1,4 @@
 // api/index.js
-import "dotenv/config";
 import app from "../src/app.js";
 
-// Vercel serverless function handler
-export default async (req, res) => {
-  // Configurar headers CORS manualmente antes de que Express los maneje
-  const origin = req.headers.origin;
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://f1-indol-theta.vercel.app"
-  ];
-  
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  }
-  
-  // Si es una petición OPTIONS, responder inmediatamente
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  
-  // Dejar que Express maneje el resto
-  return app(req, res);
-};
+export default app;
--- src/app.js
+++ src/app.js
@@ -10,43 +10,11 @@ const app = express();
 
 connectToDB().catch(err => console.error("Error Mongo:", err));
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://f1-indol-theta.vercel.app"
-];
-
-// CORS configuration
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('CORS not allowed'), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  exposedHeaders: ["Content-Length", "X-Request-Id"]
-};
-
-app.use(cors(corsOptions));
-
-// Manejo explícito de preflight para todas las rutas
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  next();
-});
+// CORS simple y directo
+app.use(cors({
+  origin: ["http://localhost:5173", "https://f1-indol-theta.vercel.app"],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(morgan("dev"));
